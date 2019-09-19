072f1b94d261239535fde1be7b02a2aadb85b5fb
NervesOfJello <cole.bonnema@loop.colum.edu>
2019-09-18 19:46:35 -0500

    Initial Branch
    Adds CRT/VCR prototyping scene, adds CRT prototype model, adds behavior scripts for buttons, CRT, and VCR

---
+++ Assets/_Project/_Scripts/ButtonBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    [HideInInspector]
+    public bool isPressed; // variable to check whether the button has been pressed
+
+    //isPressed is set back to false when its corresponding function is called in the device's script to ensure that the device "hears" when it is pressed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlayerHand") //if a player hand enters the button trigger
+        {
+            isPressed = true; //set isPressed to true
+        }
+    }
+}
---
+++ Assets/_Project/_Scripts/CRTBehavior.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CRTBehavior : MonoBehaviour
+{
+    //TODO: Refactor Signal as an inheritable interface
+    public enum Signal { Video, leftAudio, rightAudio, Power, None}; //type of signal a cable carries, including no signal
+
+    //Serialized references to the Device's Buttons
+    [SerializeField]
+    private ButtonBehavior powerButton;
+    [SerializeField]
+    private ButtonBehavior channelUpButton;
+        [SerializeField]
+    private ButtonBehavior channelDownButton;
+
+    //Serialized references to the Device's Sockets
+    [SerializeField]
+    private SocketBehavior powerSocket;
+    [SerializeField]
+    private SocketBehavior videoSocket;
+    [SerializeField]
+    private SocketBehavior leftAudioSocket;
+    [SerializeField]
+    private SocketBehavior rightAudioSocket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkButtons();
+    }
+
+    void checkButtons() //checks each of the buttons to see if they've been presse
+    {
+        if (powerButton.isPressed)
+        {
+            powerButton.isPressed = false;
+            //TODO: Add logic for if button is pressed
+        }
+
+        if (channelUpButton.isPressed)
+        {
+            channelUpButton.isPressed = false;
+            //TODO: Add logic for if button is pressed
+        }
+
+        if (channelDownButton.isPressed)
+        {
+            channelDownButton.isPressed = false;
+            //TODO: Add logic for if button is pressed
+        }
+    }
+}
