cf23b1f37d24686dcdff70b1e4f1e062319353f0
parkerhams <parker.hamilton@loop.colum.edu>
2019-09-27 09:57:38 -0500

    #29 Created DialogueManager script and Dialogue class
    Dialogue Manager handles all of the dialogue itself while the Dialogue script contains all of the information for it

---
+++ Assets/_Project/_Scripts/Dialogue.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue
+{
+    //this handles all of the dialogue starts
+    public string[] grandmaSentences;
+    public string name = "Grandma";
+}
