50307e300a82ceab32313ad220112ae7fdc681cc
parkerhams <parker.hamilton@loop.colum.edu>
2019-09-27 11:50:54 -0500

    Added dialogue trigger to a cable socket

--- Assets/_Project/_Scripts/DialogueManager.cs
+++ Assets/_Project/_Scripts/DialogueManager.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
+    [SerializeField]
+    public TextMeshProUGUI grandmaSpeechBubble;
     [SerializeField]
     private Queue<string> grandmaSentences; //first in first out system, then loads new sentence from end of queue
     private void Start()
