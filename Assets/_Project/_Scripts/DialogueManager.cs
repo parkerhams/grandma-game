213b2b021b1f808d518dc076a5f63f352a08002e
parkerhams <parker.hamilton@loop.colum.edu>
2019-09-27 12:11:12 -0500

    #29 #30 Made Dialogue Manager a Singleton

--- Assets/_Project/_Scripts/DialogueManager.cs
+++ Assets/_Project/_Scripts/DialogueManager.cs
@@ -5,17 +5,51 @@ using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
+    public static DialogueManager _dialogueManager;
+
+    //singleton behavior
+    public static DialogueManager Instance
+    {
+        get
+        {
+            if (_dialogueManager == null)
+            {
+                // Search for existing instance.
+                _dialogueManager = (DialogueManager)FindObjectOfType(typeof(DialogueManager));
+ 
+                // Create new instance if one doesn't already exist.
+                if (_dialogueManager == null)
+                {
+                    // Need to create a new GameObject to attach the singleton to.
+                    GameObject singletonObject = new GameObject();
+                    _dialogueManager = singletonObject.AddComponent<DialogueManager>();
+                    singletonObject.name = typeof(DialogueManager).ToString() + " (Singleton)";
+ 
+                    // Make instance persistent.
+                    DontDestroyOnLoad(singletonObject);
+                }
+            }
+
+            return _dialogueManager;
+        }
+ 
+        set{}
+    }
+
     [SerializeField]
     public TextMeshProUGUI grandmaSpeechBubble;
+
     [SerializeField]
     private Queue<string> grandmaSentences; //first in first out system, then loads new sentence from end of queue
     private void Start()
     {
         grandmaSentences = new Queue<string>();
+        _dialogueManager = this;
     }
 
     public void StartDialogue(Dialogue dialogue)
     {
+        
         Debug.Log("Grandma started her dialogue!");
     }
 
